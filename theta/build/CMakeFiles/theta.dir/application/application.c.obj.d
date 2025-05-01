2bd46e1cac25aa40cec7137b3724dc3823b06be0
christiansadykbayevdigipen <144493499+christiansadykbayevdigipen@users.noreply.github.com>
2025-05-01 13:46:16 -0700

    Working on gitingore

--- theta/build/CMakeFiles/theta.dir/application/application.c.obj.d
+++ theta/build/CMakeFiles/theta.dir/application/application.c.obj.d
@@ -1,10 +1,10 @@
 CMakeFiles/theta.dir/application/application.c.obj: \
- D:\repos\theta_engine\theta\src\application\application.c \
- D:\repos\theta_engine\theta\src\application\application.h \
- D:/repos/theta_engine/theta/src/core.h c:\mingw\include\stdio.h \
- c:\mingw\include\_mingw.h c:\mingw\include\msvcrtver.h \
- c:\mingw\include\w32api.h c:\mingw\include\sdkddkver.h \
- c:\mingw\include\features.h \
- c:\mingw\lib\gcc\mingw32\9.2.0\include\stddef.h \
+ C:\Users\c.sadykbayev\Desktop\Dev\repos\theta_engine\theta\src\application\application.c \
+ C:\Users\c.sadykbayev\Desktop\Dev\repos\theta_engine\theta\src\application\application.h \
+ C:/Users/c.sadykbayev/Desktop/Dev/repos/theta_engine/theta/src/core.h \
+ c:\mingw\include\stdio.h c:\mingw\include\_mingw.h \
+ c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
+ c:\mingw\include\sdkddkver.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
  c:\mingw\include\sys/types.h \
- c:\mingw\lib\gcc\mingw32\9.2.0\include\stdarg.h
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stdarg.h
--- theta/build/cmake_install.cmake
+++ theta/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/repos/theta_engine/theta/src
+# Install script for directory: C:/Users/c.sadykbayev/Desktop/Dev/repos/theta_engine/theta/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/repos/theta_engine/theta/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/c.sadykbayev/Desktop/Dev/repos/theta_engine/theta/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/repos/theta_engine/theta/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/c.sadykbayev/Desktop/Dev/repos/theta_engine/theta/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
