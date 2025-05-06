d2a3ceeb02ae30dcdf4fe6882a82644f4f7dbcdc
mouseamane <sady.chr25@gmail.com>
2025-05-05 22:50:26 -0700

    basic math library so far

--- theta/src/math/matrix.c
+++ theta/src/math/matrix.c
@@ -1,5 +1,8 @@
 #include "matrix.h"
 
+
+#include <math.h>
+
 theta_mat4x4f theta_mat4x4f_identity() {
     theta_mat4x4f matrix;
     
@@ -111,7 +114,7 @@ theta_mat4x4f theta_mat4x4f_orthographic_args(f32 left, f32 right, f32 bottom, f
     theta_mat4x4f result = {
         {
             {2.0f / (right-left), 0.0f, 0.0f, -((right + left) / (right-left))},
-            {0.0f, (2.0f / top-bottom), 0.0f, -((top+bottom)/(top-bottom))},
+            {0.0f, 2.0f / (top-bottom), 0.0f, -((top+bottom)/(top-bottom))},
             {0.0f, 0.0f, (-2.0f)/(far-near), -((far+near)/(far-near))},
             {0.0f, 0.0f, 0.0f, 1.0f}
         }
@@ -120,8 +123,92 @@ theta_mat4x4f theta_mat4x4f_orthographic_args(f32 left, f32 right, f32 bottom, f
     return result;
 }
 
-theta_mat4x4f theta_mat4x4f_perspective() {
-    theta_mat4x4f result = theta_mat4x4f_identity();
+theta_mat4x4f theta_mat4x4f_translate(theta_mat4x4f first, theta_vector3f vector) {
+    theta_mat4x4f result = theta_mat4x4f_copy(first);
+
+    result.matrix[3][0] += vector.x;
+    result.matrix[3][1] += vector.y;
+    result.matrix[3][2] += vector.z;
+
+    return result;
+}
+
+theta_mat4x4f theta_mat4x4f_rotate(theta_mat4x4f first, theta_vector3f vector) {
+    theta_mat4x4f x_component_rotation_matrix = {
+        {
+            {1.0f, 0.0f, 0.0f, 0.0f},
+            {0.0f, cos(vector.x), -sin(vector.x), 0.0f},
+            {0.0f, sin(vector.x), cos(vector.x), 0.0f},
+            {0.0f, 0.0f, 0.0f, 1.0f}
+        }
+    };
+
+    theta_mat4x4f y_component_rotation_matrix = {
+        {
+            {cos(vector.y), 0.0f, sin(vector.y), 0.0f},
+            {0.0f, 1.0f, 0.0f, 0.0f},
+            {-sin(vector.y), 0.0f, cos(vector.y), 0.0f},
+            {0.0f, 0.0f, 0.0f, 1.0f}
+        }
+    };
+
+    theta_mat4x4f z_component_rotation_matrix = {
+        {
+            {cos(vector.z), -sin(vector.z), 0.0f, 0.0f},
+            {sin(vector.z), cos(vector.z), 0.0f, 0.0f},
+            {0.0f, 0.0f, 1.0f, 0.0f},
+            {0.0f, 0.0f, 0.0f, 1.0f}
+        }
+    };
+
+    theta_mat4x4f complete_rotation_matrix = theta_mat4x4f_identity();
+    complete_rotation_matrix = theta_mat4x4f_mul(complete_rotation_matrix, z_component_rotation_matrix);
+    complete_rotation_matrix = theta_mat4x4f_mul(complete_rotation_matrix, y_component_rotation_matrix);
+    complete_rotation_matrix = theta_mat4x4f_mul(complete_rotation_matrix, x_component_rotation_matrix);
+
+    theta_mat4x4f result = theta_mat4x4f_copy(first);
+
+    result = theta_mat4x4f_mul(result, complete_rotation_matrix);
+
+    return result;
+}
+
+theta_mat4x4f theta_mat4x4f_scale(theta_mat4x4f first, theta_vector3f vector) {
+    theta_mat4x4f result = theta_mat4x4f_copy(first);
+
+    result.matrix[0][0] *= vector.x;
+    result.matrix[1][1] *= vector.y;
+    result.matrix[2][2] *= vector.z;
+
+    return result;
+}
+
+theta_mat4x4f theta_mat4x4f_scales(theta_mat4x4f first, f32 scalar) {
+    theta_mat4x4f result = theta_mat4x4f_copy(first);
+
+    result.matrix[0][0] *= scalar;
+    result.matrix[1][1] *= scalar;
+    result.matrix[2][2] *= scalar;
+
+    return result;
+}
+
+/*Honestly, this code is ripped from my previous engine, Impara2D, and I have no idea how this code works. I forgot.*/
+theta_vector3f theta_mat4x4f_transform_vector(theta_mat4x4f matrix, theta_vector3f vector) {
+    theta_vector3f result;
+
+    float oldvector[4] = {vector.x, vector.y, vector.z, 1};
+
+    float vector4[4] = {0, 0, 0, 0};
+
+    for (int i = 0; i < 4; i++)
+    {
+        vector4[i] = oldvector[0] * matrix.matrix[0][i] + oldvector[1] * matrix.matrix[1][i] + oldvector[2] + matrix.matrix[2][i] + oldvector[3] * matrix.matrix[3][i];
+    }
+
+    result.x = vector4[0] / vector4[3];
+    result.y = vector4[1] / vector4[3];
+    result.z = vector4[2] / vector4[3];
 
     return result;
 }
--- theta/src/math/matrix.h
+++ theta/src/math/matrix.h
@@ -2,6 +2,8 @@
 
 #include "core.h"
 
+#include "vector.h"
+
 /*
 @brief A 4 by 4 matrix.
 @param matrix The full 4 by 4 matrix representation.
@@ -73,18 +75,48 @@ THETA_API theta_mat4x4f theta_mat4x4f_orthographic();
 
 /*
 @brief This creates an orthographic projection matrix with special arguments.
-@param left The left value of the orthographic projection matrix.
-@param right The right value of the orthographic projection matrix.
-@param bottom The bottom value of the orthographic projection matrix.
-@param top The top value of the orthographic projection matrix.
-@param near The near value of the orthographic projection matrix.
-@param far The far value of the orthographic projection matrix.
 @returns The resulting matrix.
 */
 THETA_API theta_mat4x4f theta_mat4x4f_orthographic_args(f32 left, f32 right, f32 bottom, f32 top, f32 near, f32 far);
 
+// TODO: Add perspective projection matricies
+
 /*
-@brief This creates a perspective projection matrix.
-@returns The resulting matrix.
+@brief Translates a matrix by a vector
+@param first The matrix being translated
+@param vector The vector that the matrix is being translated by
+@returns The resulting matrix of the operation
 */
-THETA_API theta_mat4x4f theta_mat4x4f_perspective();
+THETA_API theta_mat4x4f theta_mat4x4f_translate(theta_mat4x4f first, theta_vector3f vector);
+
+/*
+@brief Rotates a matrix by a vector
+@param first The matrix being rotated
+@param vector The vector that the matrix is being rotated by
+@returns The resulting matrix of the operation
+*/
+THETA_API theta_mat4x4f theta_mat4x4f_rotate(theta_mat4x4f first, theta_vector3f vector);
+
+/*
+@brief Scales a matrix by a vector
+@param first The matrix being scaled
+@param vector The vector that the matrix is being scaled by
+@returns The resulting matrix of the operation
+*/
+THETA_API theta_mat4x4f theta_mat4x4f_scale(theta_mat4x4f first, theta_vector3f vector);
+
+/*
+@brief Scales a matrix by a scalar value
+@param first The matrix being scaled
+@param scalar The scalar value that the matrix is being scaled by
+@returns The resulting matrix of the operation
+*/
+THETA_API theta_mat4x4f theta_mat4x4f_scales(theta_mat4x4f first, f32 scalar);
+
+/*
+@brief This transforms a vector by some transformation matrix.
+@param matrix The transformation matrix to transform the vector
+@param vector The vector that is being transformed by the matrix
+@returns The resulting vector of the operation
+*/
+THETA_API theta_vector3f theta_mat4x4f_transform_vector(theta_mat4x4f matrix, theta_vector3f vector);
